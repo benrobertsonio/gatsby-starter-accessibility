d8260c4802c9b4a2aca5f5347b2779e21698bc69
Ben Robertson <benjamingrobertson@me.com>
2019-05-01 11:56:29 -0400

    Edit project meta information.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "Gatsby Accessibility Starter",
+    description:
+      "Kick off your next, great accessible Gatsby project with this starter. This starter ships with the main Gatsby configuration files you might need, as well as built-in jsx linting for accessibility errors, react-axe for catching accessibility errors in the DOM during production, and a storybook configured to test individual components for accessibility errors.",
+    author: "@gatsbyjs",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
