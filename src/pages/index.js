78af5c581efc0332841108a1b6e23571a06cfdde
Ben Robertson <benjamingrobertson@me.com>
2019-05-01 16:10:35 -0400

    Update home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,24 +14,35 @@ const IndexPage = () => (
     <p>This starter has the following accessibility features enabled:</p>
     <ul>
       <li>
+        <span role="img" aria-label="magnifying glass">
+          🔍
+        </span>
         <a href="https://github.com/evcohen/eslint-plugin-jsx-a11y">
           eslint-plugin-jsx-a11y
         </a>{" "}
         to catch potential accessibility issues while authoring code.
       </li>
       <li>
+        <span role="img" aria-label="checkbox">
+          ✅
+        </span>
         <a href="https://github.com/okonet/lint-staged">lint-staged</a>{" "}
         configuration so that any linting error, including accessibility issues,
         are caught in a pre-commit hook.
       </li>
-
       <li>
+        <span role="img" aria-label="megaphone">
+          📣
+        </span>
         <a href="https://github.com/angeloashmore/gatsby-plugin-react-axe">
           gatsby-plugin-react-axe
         </a>{" "}
         for in-console reporting of accessibility errors in the DOM.
       </li>
       <li>
+        <span role="img" aria-label="book">
+          📖
+        </span>
         A <a href="/storybook">Storybook</a> with accessibility testing and
         color blindness simulation tools built in.
       </li>
