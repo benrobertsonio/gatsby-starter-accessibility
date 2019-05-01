c1f6c17a2e0950715cec49e7cc348b0282271df9
Ben Robertson <benjamingrobertson@me.com>
2019-05-01 13:47:29 -0400

    Add some placeholder info content on the homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,32 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO
+      title="Home"
+      keywords={[`gatsby`, `application`, `react`, `accessibility`]}
+    />
+    <h1>Welcome!</h1>
+    <p>This starter has the following accessibility features enabled:</p>
+    <ul>
+      <li>
+        <a href="https://github.com/evcohen/eslint-plugin-jsx-a11y">
+          eslint-plugin-jsx-a11y
+        </a>{" "}
+        to catch potential accessibility issues while authoring code.
+      </li>
+      <li>
+        <a href="https://github.com/okonet/lint-staged">lint-staged</a>{" "}
+        configuration so that any linting error, including accessibility issues,
+        are caught in a pre-commit hook.
+      </li>
+
+      <li>
+        <a href="https://github.com/angeloashmore/gatsby-plugin-react-axe">
+          gatsby-plugin-react-axe
+        </a>{" "}
+        for in-console reporting of accessibility errors in the DOM.
+      </li>
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
